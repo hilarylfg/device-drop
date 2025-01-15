5036e1948482f64b1ebc8e0d8151c0368e50c7ec
Nikolay Polyakov <misterkola337@gmail.com>
2025-01-15 20:51:56 +0300

    upd

--- shared/components/shared/filters/filters.tsx
+++ shared/components/shared/filters/filters.tsx
@@ -1,23 +1,31 @@
 import {cn} from "@/shared/lib/utils";
 import React from "react";
-import {GroupCheckboxFilter} from "@/shared/components";
+import {Button, GroupCheckboxFilter} from "@/shared/components";
+import {RangeSlider} from "@/shared/components/ui/range-slider";
 
 export function Filters() {
     return (
-        <div className={cn('filters-block')}>
-            <h2 className="filters-block__titile">Фильтрация</h2>
-            <GroupCheckboxFilter items={[
-                {text: "Есть в наличии", value: "available"},
-                {text: "Есть скидка", value: "sale"},
-            ]}/>
-            <GroupCheckboxFilter title="Цвет" items={[
-                {text: "Белый", value: "color-white"},
-                {text: "Черный", value: "color-black"},
-                {text: "Серый", value: "color-gray"},
-                {text: "Красный", value: "color-red"},
-                {text: "Коричневый", value: "color-brown"},
-                {text: "Синий", value: "color-blue"},
-            ]}/>
+        <div className="filters-wrapper">
+            <div className={cn('filters-block')}>
+                <h2 className="filters-block__titile">Фильтрация</h2>
+                <GroupCheckboxFilter items={[
+                    {text: "Есть в наличии", value: "available"},
+                    {text: "Есть скидка", value: "sale"},
+                ]}/>
+                <div className="group-filter">
+                    <h3 className="group-filter__title">Цена</h3>
+                    <RangeSlider className="group-filter__slider" min={10} max={1000} step={10}/>
+                </div>
+                <GroupCheckboxFilter title="Цвет" items={[
+                    {text: "Белый", value: "color-white"},
+                    {text: "Черный", value: "color-black"},
+                    {text: "Серый", value: "color-gray"},
+                    {text: "Красный", value: "color-red"},
+                    {text: "Коричневый", value: "color-brown"},
+                    {text: "Синий", value: "color-blue"},
+                ]}/>
+            </div>
+            <Button>Применить</Button>
         </div>
     )
 }
